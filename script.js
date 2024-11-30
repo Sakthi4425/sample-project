ca9b71da45272099ad59c0b44337ec1e008e0439
Sakthi4425 <sakthi.c4425@gmail.com>
2024-11-30 11:11:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
-<script type="text/javascript">
-function calc()
- {
-   var m1,m2,m3,m4,avg=0,total=0,result="",grade="";
-   m1=parseInt(document.form1.s1.value);
-   m2=parseInt(document.form1.s2.value);
-   m3=parseInt(document.form1.s3.value);
-   m4=parseInt(document.form1.s4.value);
-   m5=parseInt(document.form1.s5.value);
-   total=m1+m2+m3+m4+m5;
-   avg=total/5;
-  if( m1 < 35 || m2 < 35 || m3 <35 || m4 <35 || m5 <35)
-   {
-     result="fail";
-     grade="D"; 
-    }
-  else if(avg >= 75)
-   {
-     result="distinction";
-     grade="A+";
-    }
-  else if(avg >= 60&&avg < 75)
-   {
-      result="first class";
-      grade="A";
-    }
-  else if(avg >= 50&&avg < 60)
-   {
-     result="second class";
-     grade="B";
-    }
-  else if(avg >= 35&&avg < 50)
-   {
-     result="pass class";
-     grade="C";
-    }
-  else if(avg < 35)
-   {
-     result="fail";
-     grade="D"; 
-    }
-  document.form1.result.value=result;
-  document.form1.grade.value=grade;
-  document.form1.total.value=total;
-  document.form1.average.value=avg;
+function calc() {
+  var m1, m2, m3, m4, m5, avg = 0, total = 0, result = "", grade = "";
+  m1 = parseInt(document.form.s1.value);
+  m2 = parseInt(document.form.s2.value);
+  m3 = parseInt(document.form.s3.value);
+  m4 = parseInt(document.form.s4.value);
+  m5 = parseInt(document.form.s5.value);
+  total = m1 + m2 + m3 + m4 + m5;
+  avg = total / 5;
+  if (m1 < 35 || m2 < 35 || m3 < 35 || m4 < 35 || m5 < 35) {
+      result = "fail";
+      grade = "D";
+  } else if (avg >= 75) {
+      result = "distinction";
+      grade = "A+";
+  } else if (avg >= 60 && avg < 75) {
+      result = "first class";
+      grade = "A";
+  } else if (avg >= 50 && avg < 60) {
+      result = "second class";
+      grade = "B";
+  } else if (avg >= 35 && avg < 50) {
+      result = "pass class";
+      grade = "C";
+  } else if (avg < 35) {
+      result = "fail";
+      grade = "D";
+  }
+  document.form.result.value = result;
+  document.form.grade.value = grade;
+  document.form.total.value = total;
+  document.form.average.value = avg;
 }
-</script>
